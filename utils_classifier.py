c1da86eb59b895df197c138f07892003c06dd862
TaekyungKi <1411remember@naver.com>
2021-12-16 04:39:20 +0000

    del: unused function

--- utils_classifier.py
+++ utils_classifier.py
@@ -8,20 +8,6 @@ import pandas as pd
 
 from enum import Enum
 
-import torch
-
-from torch.utils.data import Dataset, DataLoader
-from torch.utils.data import WeightedRandomSampler
-
-def format_time(elapsed):
-    elapsed_rounded = int(round((elapsed)))
-    return str(datetime.timedelta(seconds=elapsed_rounded))
-
-def flat_accuracy(preds, labels):
-    pred_flat = np.argmax(preds, axis=1).flatten()
-    labels_flat = labels.flatten()
-    return np.sum(pred_flat == labels_flat) / len(labels_flat)
-
 class QType8(Enum):
     Arithmetic = 0
     Ordering = 1
